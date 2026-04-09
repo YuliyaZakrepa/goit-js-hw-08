7616564fd9f817c4d8bf3e8ef079d46630a60f4f
Yuliya Zakrepa <yv.zakrepa@gmail.com>
2026-04-09 15:50:38 +0300

    task 8

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputText = document.querySelector("#name-input");
-const outputText = document.querySelector("#name-output");
-console.log(inputText);
-inputText.addEventListener("input", handleInput);
-function handleInput(event) {
-  const valueInputText = event.target.value.trim();
-
-  outputText.textContent = valueInputText === "" ? "Anonymous" : valueInputText;
-}
